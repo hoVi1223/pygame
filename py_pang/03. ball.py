e747c5e90bd557231bd47c00100704eccca35361
NA <rahs0120@gmail.com>
2020-10-10 15:50:59 +0900

    무기 처리

--- py_pang/03. ball.py
+++ py_pang/03. ball.py
@@ -6,6 +6,9 @@ import os
 #파이게임 초기화
 pygame.init()
 
+#프레임 설정
+clock = pygame.time.Clock()
+
 #화면 생성
 screen_width = 640
 screen_height = 480
@@ -30,17 +33,57 @@ character_width = character_size[0]
 character_height = character_size[1]
 character_x_pos = screen_width // 2 - character_width // 2
 character_y_pos = screen_height - stage_height - character_height
+character_speed = 0
+
+#무기 이미지
+weapon = pygame.image.load(os.path.join(img_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+weapon_speed = 10
+
+#무기 저장소
+weapons = []
+
 
 #게임 루프
 running = True
 while running:
+    dt = clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_speed -= 5
+            elif event.key == pygame.K_RIGHT:
+                character_speed += 5
+            elif event.key == pygame.K_SPACE:
+                weapon_x_pos = character_x_pos + character_width // 2 - weapon_width // 2
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_speed = 0
+
+
+    character_x_pos += character_speed
+    if character_x_pos < 0:
+        character_x_pos = 0
+    if character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    #무기 위치 업데이트
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
+
+    #천장에 닿은 무기는 제거하기
+    weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
+
     screen.blit(bg, (0, 0))
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
+    for one in weapons:
+        screen.blit(weapon, (one[0], one[1]))
     pygame.display.update()
 
 #파이 게임 종료
