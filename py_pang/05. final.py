b889a4e58d3278a4e2aa1bb3faa809a122e00453
NA <rahs0120@gmail.com>
2020-10-24 14:33:02 +0900

    3. 캐릭터 이동

--- py_pang/05. final.py
+++ py_pang/05. final.py
@@ -1,9 +1,5 @@
 # 05. final.py
 
-# 파이팡 게임 만들기
-import pygame
-import os
-
 # 01. 파이 게임 초기화
 # 02. 게임 환경 초기화
 # 03. 캐릭터 이동
@@ -13,8 +9,15 @@ import os
 # 07. 공 나누기
 # 08. 메시지 출력
 
+# 파이팡 게임 만들기
+import pygame
+import os
+
 # 파이 게임 초기화
 pygame.init()
+
+# FPS (Frame Per Second)
+clock = pygame.time.Clock()
 
 screen_width = 640
 screen_height = 480
@@ -37,18 +40,32 @@ character_width = character_rect[0]
 character_height = character_rect[1]
 character_pos_x = screen_width // 2 - character_width // 2
 character_pos_y = screen_height - stage_height - character_height
+character_speed = 0
 
 # 게임 루프
 running = True
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_speed -= 5
+            elif event.key == pygame.K_RIGHT:
+                character_speed += 5
+        if event.type == pygame.KEYUP:
+            character_speed = 0
+    character_pos_x += character_speed
+
+    if character_pos_x < 0: character_pos_x = 0
+    if character_pos_x > screen_width - character_width:
+        character_pos_x = screen_width - character_width
+
     # 화면 출력
     screen.blit(background_img, (0, 0))
     screen.blit(stage_img, (0, screen_height - stage_height))
     screen.blit(character_img, (character_pos_x, character_pos_y))
-
     # 화면 업데이트
     pygame.display.update()
 
