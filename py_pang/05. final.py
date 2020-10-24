506093c45a092e483b527c875e0e2e46f59243ff
NA <rahs0120@gmail.com>
2020-10-24 14:24:04 +0900

    1. 게임 환경 초기화

--- py_pang/05. final.py
+++ py_pang/05. final.py
@@ -1,6 +1,6 @@
-#05. final.py
+# 05. final.py
 
-#파이팡 게임 만들기
+# 파이팡 게임 만들기
 import pygame
 import os
 
@@ -13,7 +13,7 @@ import os
 # 07. 공 나누기
 # 08. 메시지 출력
 
-#파이 게임 초기화
+# 파이 게임 초기화
 pygame.init()
 
 screen_width = 640
@@ -22,15 +22,35 @@ screen_size = (screen_width, screen_height)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("PYPANG!!")
 
-#게임 루프
+# 이미지 불러오기
+cur_path = os.path.dirname(__file__)
+img_path = os.path.join(cur_path, "images")
+
+background_img = pygame.image.load(os.path.join(img_path, "background.png"))
+
+stage_img = pygame.image.load(os.path.join(img_path, "stage.png"))
+stage_height = stage_img.get_rect().size[1]
+
+character_img = pygame.image.load(os.path.join(img_path, "character.png"))
+character_rect = character_img.get_rect().size
+character_width = character_rect[0]
+character_height = character_rect[1]
+character_pos_x = screen_width // 2 - character_width // 2
+character_pos_y = screen_height - stage_height - character_height
+
+# 게임 루프
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # 화면 출력
+    screen.blit(background_img, (0, 0))
+    screen.blit(stage_img, (0, screen_height - stage_height))
+    screen.blit(character_img, (character_pos_x, character_pos_y))
 
-    #화면 업데이트
+    # 화면 업데이트
     pygame.display.update()
 
-#파이 게임 종료
+# 파이게임 종료
 pygame.quit()
