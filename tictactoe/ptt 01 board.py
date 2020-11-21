edf5804f890323d2664f56d4aa6168fa1698170f
NA <rahs0120@gmail.com>
2020-11-21 14:35:56 +0900

    1. 파이게임 초기화

---
+++ tictactoe/ptt 01 board.py
@@ -0,0 +1,23 @@
+#ptt 01
+
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 450
+SCREEN_HEIGHT = 450
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+#파이게임 화면 초기화
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("TIC TAC TOE")
+
+#게임 루프
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+#파이게임 종료
+pygame.quit()
